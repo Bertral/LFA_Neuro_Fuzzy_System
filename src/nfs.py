a3e733fdf3f968044163c1b77b676ef954d28a23
Antoine Friant <antoine.friant@hotmail.com>
2018-05-26 18:50:58 +0200

    observation count for each class and feature

--- src/nfs.py
+++ src/nfs.py
@@ -1,33 +1,66 @@
 import numpy as np
 from src.fuzzy_systems.core.rules.fuzzy_rule import FuzzyRule
+from sklearn import datasets
 
 
 class NFS():
-    def __init__(self, max_rules: int, min_observations_per_rule: int):
+    def __init__(self, max_rules: int = 10, min_observations_per_rule: int = 5):
         self._max_rules = max_rules
         self._min_observations_per_rule = min_observations_per_rule
         self._rules = None
-        self._membership_functions = None
 
-    def train(self, data: np.ndarray, nb_iter: int):
+    def train(self, data: np.ndarray, target: np.ndarray, nb_iter: int):
         """
         Train the NFS for nb_iter complete passes on data
         Data must be a 2-dimentionnal numpy array of this format :
         
-        value_A_of_1    value_B_of_1    ...     value_X_of_1    class_of_1
-        value_A_of_2    value_B_of_2    ...     value_X_of_2    class_of_2
-        ...             ...             ...     ...             ...
-        value_A_of_3    value_B_of_3    ...     value_X_of_3    class_of_3
+        value_A_of_1    value_B_of_1    ...     value_X_of_1
+        value_A_of_2    value_B_of_2    ...     value_X_of_2
+        ...             ...             ...     ...
+        value_A_of_n    value_B_of_n    ...     value_X_of_n
         
         """
-        self._rules = None
-        self._membership_functions = None
+        self._rules = []
 
-        for feature in range(0, data.shape[1]):
+        membership_functions = {}
+
+        # for each feature, initialize memberships functions
+        for feat_index in range(0, np.shape(data)[1]):
+            min = np.min(data[:, feat_index])
+            max = np.max(data[:, feat_index])
+            membership_functions[feat_index] = {}
+
+            # split dataset equally based on current feature
+            splits = {}
             for n in range(0, 5):
-                "TODO : create 5 MV for the feature"
-            "TODO : create as many MF as there are features"
+                splits[(min + n*(max - min)/5, min + (n + 1)*(max - min)/5)] = {}
+
+            # for each slice, count the number of observations of each class inbetween splits
+            for observation in range(0, np.shape(data)[0]):
+                for (k_low, k_high) in splits.keys():
+                    if k_low <= data[observation, feat_index] <= k_high:
+
+                        # if class dict not initialized, create it
+                        if target[observation] not in splits[(k_low, k_high)]:
+                            splits[(k_low, k_high)][target[observation]] = 0
+
+                        # increment the count for this class, slice and feature
+                        splits[(k_low, k_high)][target[observation]] += 1
+                        break
+            membership_functions[feat_index] = splits
+
+        # membership functions now is membership_function[feature_number][(slice_from, slice_to)][class] = observ. count
+
+        # TODO find the slice intersections that contain more than min_observations_per_rule of a single class
+        # then start learning
+
+        print(membership_functions)
 
     def inspect(self):
-        "nothing yet"
-        # TODO : print rules
+        "TODO print FIS"
+
+
+# test script
+nfs = NFS()
+iris = datasets.load_iris()
+nfs.train(iris.data, iris.target, 100)
